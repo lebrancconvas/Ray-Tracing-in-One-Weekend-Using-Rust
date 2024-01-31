7ba502ab6f9c48987a32c8d038369e15a0bbc3d5
lebrancconvas <lebrancconvas@gmail.com>
2024-01-31 19:26:26 +0700

    ✨ feat: update config from method to module

--- src/config/config.rs
+++ src/config/config.rs
@@ -7,11 +7,13 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn set(&mut self, aspect_ratio: f64, image_width: u32, viewport_height: u32) {
-    self.aspect_ratio = aspect_ratio;
-    self.image_width = image_width;
-    self.image_height = (image_width as f64 / aspect_ratio) as u32;
-    self.viewport_height = viewport_height;
-    self.viewport_width = viewport_height * (image_width as f64 / image_height as f64) as u32;
+  pub fn set(aspect_ratio: f64, image_width: u32, viewport_width: u32) -> Config {
+    Config {
+      aspect_ratio,
+      image_width,
+      image_height: (image_width as f64 / aspect_ratio) as u32,
+      viewport_width,
+      viewport_height: (viewport_height / (image_width as f64 / image_height as f64)) as u32
+    }
   }
 }
