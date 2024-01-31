70ecb9f2f613467eb3ac4c5854ec4077c3822742
lebrancconvas <lebrancconvas@gmail.com>
2024-01-31 19:29:47 +0700

    ✨ feat: update config

--- src/config/config.rs
+++ src/config/config.rs
@@ -1,9 +1,9 @@
 pub struct Config {
-  aspect_ratio: f64,
-  image_width: u32,
-  image_height: u32,
-  viewport_width: u32,
-  viewport_height: u32
+  pub aspect_ratio: f64,
+  pub image_width: u32,
+  pub image_height: u32,
+  pub viewport_width: u32,
+  pub viewport_height: u32
 }
 
 impl Config {
@@ -13,7 +13,7 @@ impl Config {
       image_width,
       image_height: (image_width as f64 / aspect_ratio) as u32,
       viewport_width,
-      viewport_height: (viewport_height / (image_width as f64 / image_height as f64)) as u32
+      viewport_height: (viewport_width as f64 / (image_width as f64 /  (image_width as f64 / aspect_ratio))) as u32
     }
   }
 }
